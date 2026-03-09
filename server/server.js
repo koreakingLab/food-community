aa5336aaaadebb502ce97796db2ac8f789c6b540
koreakingLab <choseonje@gmail.com>
2026-03-09 11:02:55 +0900

    haccp route use add

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
+const haccpRoutes = require('./routes/haccp');
+app.use(haccpRoutes);   
 
 // ===== 미들웨어 =====
 app.use(cors());
