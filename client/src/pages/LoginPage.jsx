615ca1628b3b80c992eaf420de205ae52a0abbfc
koreakingLab <choseonje@gmail.com>
2026-03-12 15:57:24 +0900

    delete IconEye, IconLock import code

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { IconEye, IconLock } from '../components/Icons';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
